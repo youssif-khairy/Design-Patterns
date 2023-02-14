c7fc98babf6f0f72719f5baea8f04ef3530797a2
Youssif Khairy <Youssif.Khairy@codelabsys.com>
2023-02-14 09:59:40 +0200

    implement singleton pattern

---
+++ Singleton_Pattern/President.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    public class President
+    {
+        private static President? _president;
+
+        private static object locking = new object();
+        private President() { }
+
+        public static President CreatePresident()
+        {
+            lock (locking)
+            {
+                if (_president == null)
+                {
+                    return _president = new President();
+                }
+            }
+            return _president;
+        }
+    }
+}
---
+++ Singleton_Pattern/Program.cs
@@ -0,0 +1,9 @@
+﻿//Creational DP
+
+
+using Singleton_Pattern;
+
+var x = President.CreatePresident();
+var y = President.CreatePresident();
+
+Console.WriteLine(x == y ? "Same Instances" : "Diffrent Instances");
