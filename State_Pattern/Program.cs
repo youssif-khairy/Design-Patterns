f79f117112e8fc8f338021d263f220234c9e8d53
Youssif Khairy <Youssif.Khairy@codelabsys.com>
2023-02-15 16:20:42 +0200

    Add example for state pattern

--- State_Pattern/Program.cs
+++ State_Pattern/Program.cs
@@ -10,9 +10,14 @@
 y3ny el composition mwgod fl state fy el Istate we el Main class
 
 
+
+
 // state is extension of strategy
  * */
 
+// All Sub states is treated as one state so all treated from player as it contain one state 
+// So when working with state please make all data properties in Abstract state so that it will be shared accross all states
+//example : https://www.dofactory.com/net/state-design-pattern
 
 using State_Pattern;
 
